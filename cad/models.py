9abbb1ca475dd62a312c4194d4d77ee1035599b9
musafar <musafarshakeel@gmail.com>
2022-11-14 23:59:55 +0530

    added table for driver-client

--- cad/models.py
+++ cad/models.py
@@ -9,6 +9,18 @@ class user_details(models.Model):
     password = models.CharField(max_length=50)
     age = models.CharField(max_length=5)
     gender = models.CharField(max_length=10)
+    Firebase_id = models.CharField(max_length=100)
     created_on = models.DateTimeField(auto_now_add=True)
 
+class driver_detail(models.Model):
+    driver_id = models.AutoField(primary_key=True)
+    d_fname = models.CharField(max_length=50)
+    d_lname = models.CharField(max_length=50)
+    d_email = model.EmailField(max_length=50)
+    d_phone = models.CharField(max_length=15)
+    d_password = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10)
+    d_photo=models.CharField(max_length=100)
+    D_firebaseid = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True)
 
